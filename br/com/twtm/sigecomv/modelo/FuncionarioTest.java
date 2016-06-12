b2d0acac8d9e1350768bd330ba6666f8176142db
Marcos Paulo da Silva <mpsacademico@gmail.com>
2016-06-12 11:14:10 -0300

    Desmembramento dos testes da classe Funcionario

--- br/com/twtm/sigecomv/modelo/FuncionarioTest.java
+++ br/com/twtm/sigecomv/modelo/FuncionarioTest.java
@@ -9,25 +9,73 @@ public class FuncionarioTest extends TestCase{
     }
     
     @Test
-    public void testValCpfV(){        
-        assertTrue(Funcionario.valCpf("43754998854"));
-        assertTrue(Funcionario.valCpf("19337264749"));
-        assertTrue(Funcionario.valCpf("42159270130"));
-        assertTrue(Funcionario.valCpf("77568893120"));
+    public void testValCpfV01(){        
+        assertTrue(Funcionario.valCpf("43754998854"));        
+    }
+    
+    @Test
+    public void testValCpfV02(){                
+        assertTrue(Funcionario.valCpf("19337264749"));        
+    }
+    
+    @Test
+    public void testValCpfV03(){       
+        assertTrue(Funcionario.valCpf("42159270130"));        
+    }
+    
+    @Test
+    public void testValCpfV04(){               
+        assertTrue(Funcionario.valCpf("77568893120"));        
+    }
+    
+    @Test
+    public void testValCpfV05(){       
         assertTrue(Funcionario.valCpf("35017776800"));
     }
     
     @Test
-    public void testValCpfF(){
-        assertFalse(Funcionario.valCpf("11111111111"));
-        assertFalse(Funcionario.valCpf("22222222222"));
-        assertFalse(Funcionario.valCpf("33333333333"));
-        assertFalse(Funcionario.valCpf("44444444444"));
-        assertFalse(Funcionario.valCpf("55555555555"));
-        assertFalse(Funcionario.valCpf("41542185454"));
-        assertFalse(Funcionario.valCpf("2516513215545"));
-        assertFalse(Funcionario.valCpf("2516514545"));
-        assertFalse(Funcionario.valCpf("2"));        
+    public void testValCpfF01(){
+        assertFalse(Funcionario.valCpf("11111111111"));            
+    }
+    
+    @Test
+    public void testValCpfF02(){        
+        assertFalse(Funcionario.valCpf("22222222222"));             
     }   
+    
+    @Test
+    public void testValCpfF03(){        
+        assertFalse(Funcionario.valCpf("33333333333"));            
+    }   
+    
+    @Test
+    public void testValCpfF04(){        
+        assertFalse(Funcionario.valCpf("44444444444"));         
+    }   
+    
+    @Test
+    public void testValCpfF05(){        
+        assertFalse(Funcionario.valCpf("55555555555"));         
+    }   
+    
+    @Test
+    public void testValCpfF06(){        
+        assertFalse(Funcionario.valCpf("41542185454"));           
+    }   
+    
+    @Test
+    public void testValCpfF07(){        
+        assertFalse(Funcionario.valCpf("2516513215545"));         
+    }   
+    
+    @Test
+    public void testValCpfF08(){        
+        assertFalse(Funcionario.valCpf("2516514545"));
+    }   
+    
+    @Test
+    public void testValCpfF09(){            
+        assertFalse(Funcionario.valCpf("2"));        
+    }  
     
 }
